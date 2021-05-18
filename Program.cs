1fb8ff431587325d0e9d60c75e19195132e8a92c
Hugo Ferreira <46359390+hugofer19@users.noreply.github.com>
2021-05-18 22:24:12 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Área
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declarar variaveis
+            int comprimento, altura, area;
+
+            // obter dados
+            Console.Write("Comprimento:");
+            comprimento = int.Parse(Console.ReadLine());
+            Console.Write("Altura:");
+            altura = int.Parse(Console.ReadLine());
+
+            //calcular
+            area = comprimento * altura;
+
+            //apresentar resultado
+            Console.WriteLine("Área:" + area);
+            Console.Read();
+        }
+    }
+}
